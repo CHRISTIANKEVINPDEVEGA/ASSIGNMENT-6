5b17574d7ff21087d4025aa1491decd15f2b63e0
CHRISTIAN KEVIN P. DE VEGA <christiandevega12@gmail.com>
2021-11-26 22:11:05 +0800

    -fixed some bugs, -change some codes to look more oraganize

--- Program1_NumberSorter.py
+++ Program1_NumberSorter.py
@@ -9,56 +9,56 @@ def ask_for_number():
 
 def display(FiN,SeN,ThN,FoN):
     print("The numbers are arrange in a descending order:")
-    if FiN >= SeN and SeN >= ThN and ThN >= FoN:#1234
+    if FiN >= SeN >= ThN >= FoN:#1234
         print(f"{FiN},{SeN},{ThN},{FoN}") 
-    elif FiN >= ThN and ThN >= SeN and SeN >= FoN:#1324
+    elif FiN >= ThN >= SeN >= FoN:#1324
         print(f"{FiN},{ThN},{SeN},{FoN}")
-    elif FiN >= FoN and FoN >= SeN and SeN >= ThN:#1423
+    elif FiN >= FoN >= SeN >= ThN:#1423
         print(f"{FiN},{FoN},{SeN},{ThN}")
-    elif FiN >= SeN and SeN >= FoN and FoN >= ThN:#1243
+    elif FiN >= SeN >= FoN >= ThN:#1243
         print(f"{FiN},{SeN},{FoN},{ThN}") 
-    elif FiN >= ThN and ThN >= FoN and FoN >= SeN:#1342
+    elif FiN >= ThN >= FoN >= SeN:#1342
         print(f"{FiN},{ThN},{FoN},{SeN}")
-    elif FiN >= FoN and FoN >= ThN and ThN >= SeN:#1432
+    elif FiN >= FoN >= ThN >= SeN:#1432
         print(f"{FiN},{FoN},{ThN},{SeN}") 
     else:
-        if SeN >= FiN and FiN >= ThN and ThN >= FoN:#2134
+        if SeN >= FiN >= ThN >= FoN:#2134
            print(f"{SeN},{FiN},{ThN},{FoN}") 
-        elif SeN >= ThN and ThN >= FiN and FiN >= FoN:#2314
+        elif SeN >= ThN >= FiN >= FoN:#2314
            print(f"{SeN},{ThN},{FiN},{FoN}")  
-        elif SeN >= FoN and FoN >= FiN and FiN >= ThN:#2413
+        elif SeN >= FoN >= FiN >= ThN:#2413
            print(f"{SeN},{FoN},{FiN},{ThN}") 
-        elif SeN >= FiN and FiN >= FoN and FoN >= ThN:#2143
+        elif SeN >= FiN >= FoN >= ThN:#2143
            print(f"{SeN},{FiN},{FoN},{ThN}") 
-        elif SeN >= ThN and ThN >= FoN and FoN >= FiN:#2341
+        elif SeN >= ThN >= FoN >= FiN:#2341
            print(f"{SeN},{ThN},{FoN},{FiN}") 
-        elif SeN >= FoN and FoN >= ThN and ThN >= FiN:#2431
+        elif SeN >= FoN >= ThN >= FiN:#2431
            print(f"{SeN},{FoN},{ThN},{FiN}") 
         else:
-            if ThN >= FiN and FiN >= SeN and SeN >= FoN: #3124
+            if ThN >= FiN >= SeN >= FoN: #3124
                print(f"{ThN},{FiN},{SeN},{FoN}")
-            elif ThN >= SeN and SeN >= FiN and FiN >= FoN: #3214
+            elif ThN >= SeN >= FiN >= FoN: #3214
                print(f"{ThN},{SeN},{FiN},{FoN}")
-            elif ThN >= FoN and FoN >= FiN and FiN >= SeN: #3412
-               print(f"{ThN},{FiN},{SeN},{FoN}")
-            elif ThN >= FiN and FiN >= FoN and FoN >= SeN: #3142
+            elif ThN >= FoN >= FiN >= SeN: #3412
+               print(f"{ThN},{FoN},{FiN},{SeN}")
+            elif ThN >= FiN  >= FoN >= SeN: #3142
                print(f"{ThN},{FiN},{FoN},{SeN}")
-            elif ThN >= SeN and SeN >= FoN and FoN >= FiN: #3241
+            elif ThN >= SeN >= FoN >= FiN: #3241
                print(f"{ThN},{SeN},{FoN},{FiN}")
-            elif ThN >= FoN and FoN >= SeN and SeN >= FiN: #3421
+            elif ThN >= FoN >= SeN >= FiN: #3421
                print(f"{ThN},{FoN},{SeN},{FiN}")
             else:
-                if FoN >= FiN and FiN >= SeN and SeN >= ThN: #4123
+                if FoN >= FiN >= SeN >= ThN: #4123
                    print(f"{FoN},{FiN},{SeN},{ThN}")
-                elif FoN >= SeN and SeN >= FiN and FiN >= ThN: #4213
+                elif FoN >= SeN >= FiN >= ThN: #4213
                    print(f"{FoN},{SeN},{FiN},{ThN}")  
-                elif FoN >= ThN and ThN >= FiN and FiN >= SeN: #4312
+                elif FoN >= ThN >= FiN >= SeN: #4312
                    print(f"{FoN},{ThN},{FiN},{SeN}")  
-                elif FoN >= FiN and FiN >= ThN and ThN >= SeN: #4132
+                elif FoN >= FiN >= ThN >= SeN: #4132
                    print(f"{FoN},{FiN},{ThN},{SeN}")  
-                elif FoN >= FiN and FiN >= SeN and SeN >= FiN: #4231
+                elif FoN >= SeN >= ThN >= FiN: #4231
                    print(f"{FoN},{SeN},{ThN},{FiN}")  
-                elif FoN >= FiN and FiN >= SeN and SeN >= FiN: #4321
+                elif FoN >= ThN >= SeN >= FiN: #4321
                    print(f"{FoN},{ThN},{SeN},{FiN}")   
 
 
